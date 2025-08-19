ea4b7f3456eeb306ca097447b4a57269e2715cb5
Prashant Rastogi <publicidforpeople>
2025-08-20 00:30:43 +0530

    add comments and make it human readable code

--- index.js
+++ index.js
@@ -1,73 +1,99 @@
 const todoList = document.getElementById("todoList");
 const addButton = document.getElementById("addButton");
-const taskInput = document.getElementById("taskInput");
+const input = document.getElementById("taskInput");
 loadTasks();
-function createElement(tagName, className, textContent) {
+
+function createElement(tagName, textContent) {
   const element = document.createElement(tagName);
-  element.className = className;
-  if (textContent) element.appendChild(document.createTextNode(textContent));
+
+  if (textContent) {
+    element.appendChild(document.createTextNode(textContent));
+  }
+
   return element;
 }
+
 function createCard(inputId, taskContent) {
-  const listItem = createElement("li", "todo-item");
-  const checkbox = createElement("input", "task-checkbox");
-  const taskLabel = createElement("label", "task-label", taskContent);
-  const closeButton = createElement("button", "btn-close", "Close");
+  // ELEMENTS
+  const li = createElement("li");
+  const checkbox = createElement("input");
+  const label = createElement("label", taskContent);
+  const button = createElement("button", "Close");
+
+  // CLASSNAME
+  li.className = `todo-item`;
+  checkbox.className = `task-checkbox`;
+  label.className = `task-label`;
+  button.className = `btn-close`;
+
+  // ATTRIBUTES
   checkbox.type = "checkbox";
   checkbox.id = inputId;
-  taskLabel.htmlFor = inputId;
+  label.htmlFor = inputId;
 
+  // EVENT LISTENERS
   checkbox.addEventListener("change", () => {
-    taskLabel.style.textDecoration = checkbox.checked ? "line-through" : "none";
+    label.style.textDecoration = checkbox.checked ? "line-through" : "none";
   });
 
-  closeButton.addEventListener("click", (e) => {
+  button.addEventListener("click", (e) => {
     e.target.parentElement.remove();
     saveTasks();
   });
 
-  listItem.append(checkbox, taskLabel, closeButton);
-  return listItem;
+  // APPEND ELEMENTS
+  li.append(checkbox, label, button);
+  return li;
 }
 
 const newTaskId = `task-${todoList.children.length + 1}`;
-taskInput.addEventListener(`keyup`, (e) => {
+
+input.addEventListener(`keyup`, (e) => {
   const taskValue = e.target.value.trim();
+
   if (e.key == "Enter" && e.target.value !== "") {
     todoList.appendChild(createCard(newTaskId, taskValue));
     e.target.value = "";
+
     saveTasks();
   }
 });
 
 addButton.addEventListener("click", () => {
-  const taskValue = taskInput.value.trim();
+  const taskValue = input.value.trim();
   if (taskValue !== "") {
     todoList.appendChild(createCard(newTaskId, taskValue));
-    taskInput.value = "";
+    input.value = "";
+
     saveTasks();
   }
 });
 
 function saveTasks() {
   const tasks = [];
-  const listItem = todoList.getElementsByClassName(`todo-item`);
-  for (let index = 0; index < listItem.length; index++) {
-    const element = listItem[index];
-    const taskLabel = element.querySelector(".task-label");
-    if (taskLabel) {
-      const taskContent = taskLabel.textContent;
+  const li = todoList.getElementsByClassName(`todo-item`);
+
+  for (let index = 0; index < li.length; index++) {
+    const element = li[index];
+    const label = element.querySelector(".task-label");
+
+    if (label) {
+      const taskContent = label.textContent;
       tasks.push(taskContent);
     }
   }
+
   localStorage.setItem("keys", JSON.stringify(tasks));
 }
+
 function loadTasks() {
   const tasks = JSON.parse(localStorage.getItem("keys")) || [];
+
   for (let index = 0; index < tasks.length; index++) {
     const taskContent = tasks[index];
-    const taskLabel = `task-${index + 1}`;
-    const taskItem = createCard(taskLabel, taskContent);
+    const label = `task-${index + 1}`;
+    const taskItem = createCard(label, taskContent);
+
     todoList.appendChild(taskItem);
   }
 }
